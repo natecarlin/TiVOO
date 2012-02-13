3804167e075439b11c9530b373a2b3e6f5cf1ca7
Antares Yee <antaresyee@gmail.com>
2012-02-13 11:41:19 -0500

    edited comment in Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,9 @@ public class Main {
 	
 	public static void main (String args[]) throws IOException {
 		ArrayList<Event> myEvents = xmlParse.parse("http://www.cs.duke.edu/courses/cps108/current/assign/02_tivoo/data/dukecal.xml");
-		
 		myEvents=new xmlProcess().process(myEvents);
-		//TODO: Gang or Nathan: implement the Processing part.  The input is myEvents, the output should also be myEvents.
 		htmlOutput.makeOutput(myEvents);
+		
 		System.out.println(myEvents.toString());
 	}
 }
