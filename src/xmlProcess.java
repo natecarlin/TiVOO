a2a5ae6cea7a19ebeee4bcfce4093871a43efc84
Gang Song <gang.song@duke.edu>
2012-02-12 21:24:00 -0500

    Added a xmlProcess class to filter event in a simple way

---
+++ src/xmlProcess.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+
+public class xmlProcess {
+
+	public  ArrayList<Event> process(ArrayList<Event> myEvent){
+		
+		myEvent=keywordFilter(myEvent);
+
+		return myEvent;		
+	}
+
+	/*
+	 * use keyword to filter events
+	 */
+	private ArrayList<Event> keywordFilter(ArrayList<Event> myEvent){
+		ArrayList<Event> temp = new ArrayList<Event>();
+		for(Event current: myEvent){
+			if(current.toString().contains("meet")){
+				temp.add(current);
+			}			
+		}
+		return temp;
+
+	}
+	
+	public static void main (String[] args){
+		
+		ArrayList<Event> test =new ArrayList<Event>();
+		test.add(new Event("meet with TA", "bbb", "cd", 8, 10));
+		test.add(new Event("talk with TA", "bbb", "cd", 8, 10));
+		test=new xmlProcess().process(test);
+		
+		for(Event m: test){
+			System.out.println(m.toString());
+		}
+	}
+}
